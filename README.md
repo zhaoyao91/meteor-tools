25ec0147b25c33a99557a55d88faf1f5595e8e3b
zhaoyao91 <zhaoyao91@163.com>
2016-11-10 03:04:47 +0800

    update script

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ RUN /tools/install_base.sh \
   && /tools/install_meteor.sh \
   && /tools/build_app.sh /src /app \
   && /tools/prepare_app.sh /app \
-  && /tools/cleanup.sh && rm -rf /src
+  && /tools/cleanup.sh \
+  && rm -rf /src
 
 CMD /tools/run_app.sh /app
 
--- tools/build_app.sh
+++ tools/build_app.sh
@@ -3,18 +3,16 @@
 app_src=$1
 app_dest=$2
 
-temp_src=/tmp/src
 temp_bundle=/tmp/bundle
 
 # build app
-cp -ar ${app_src} ${temp_src}
-cd ${temp_src}
+cd ${app_src}
 meteor npm install
-meteor build --directory ${temp_bundle} || \
-meteor build --directory ${temp_bundle} --unsafe-perm
+meteor build --directory ${temp_bundle} --allow-superuser || \
+meteor build --directory ${temp_bundle} --unsafe-perm || \
+meteor build --directory ${temp_bundle}
 
 mv ${temp_bundle}/bundle ${app_dest}
 
 # clean up
-rm -rf ${temp_src}
 rm -rf ${temp_bundle}
